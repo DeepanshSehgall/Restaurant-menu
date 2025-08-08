293c52d19ef49b72d007f961eefd5fbc447d12e9
DeepanshSehgall <deepansh1602@gmail.com>
2025-08-08 20:03:35 +0530

    Add files via upload

---
+++ react.js
@@ -0,0 +1,33 @@
+// // import React from 'react'
+
+// const App=()=>{
+//   return(
+//    <>
+//    <MyName/>
+//    <MyName/>
+//    <MyName/>
+//    <MyName/>
+//    <h1 className="myclassname">Hello World {3+3}</h1>
+//    <p>Awesome</p>
+//    <img src="" alt=""></img>
+//   </>
+//   );
+// };
+
+// const MyName=()=>{
+//   return <hi>Vinod Bahadur Thapa</hi>
+// };
+
+// export default App;
+
+
+
+import React from "react"
+import Restaurant from "./Restaurant";
+
+const App = () => {
+  return  <Restaurant/>;
+};
+
+export default App;
+
